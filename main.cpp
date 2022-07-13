4bd75585c84376a8eb6f85d2b2ac45ae9acb2383
Marco Mendez <marcomendez3800@gmail.com>
2022-07-12 23:16:19 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     glutInit(&argc, argv);                                      //Initialize GLUT
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);   //Open an OpenGL context with double buffering, RGB colors, and depth buffering
     glutInitWindowSize(Window::width, Window::height);          //Set initial window size
-    glutCreateWindow("UCSD CSE 167 - Project 5 - Robot Army"); //Open window and set window title
+    glutCreateWindow("OpenGL Robot Army"); //Open window and set window title
     
     glEnable(GL_DEPTH_TEST);                                    //Enable depth buffering
     glClear(GL_DEPTH_BUFFER_BIT);                               //Clear depth buffer
